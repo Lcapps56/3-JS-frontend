edc45fdb55bfe5546dd41bcb9c8938746339c75a
Leland Capps <lcapps5600@gmail.com>
2022-12-13 22:55:51 -0500

    more JS, added to save a note in localStorage

--- Notes app/js/logic.js
+++ Notes app/js/logic.js
@@ -3,6 +3,8 @@
 const addBtn = document.querySelector('.add-box')
 const popup = document.querySelector('.popup-box')
 const popupClose = document.querySelector('.close')
+// Get notes from localstorage and parse it to JS or pass an empty array as notes
+const notes = JSON.parse(localStorage.getItem('notes') || "[]")
 
 addBtn.addEventListener('click', () => {
     popup.style.display = 'block'
@@ -17,21 +19,34 @@ popupClose.addEventListener('click', () => {
 const addNote = document.querySelector('.addNote')
 const titleTag = document.querySelector('#title')
 const descTag = document.querySelector('#desc')
+const months = ['January', "February", 'March', 'April', 'May', "June", 'July', 'August', 'September', 'October', 'November', "December"]
 
 addNote.addEventListener('click', (e) => {
     e.preventDefault()
     let noteTitle = titleTag.value 
     let noteDesc = descTag.value
     if(noteTitle || noteDesc){
-        console.log(noteTitle, noteDesc)
+        // getting date
+        let dateObj = new Date()
+        let month = months[dateObj.getMonth()]
+        let day = dateObj.getDay()
+        let year = dateObj.getFullYear()
+        let dateStr = `${month} ${day}, ${year}`
+
+        let newNote = {
+            title: noteTitle,
+            desc: noteDesc,
+            date: dateStr
+        }
+        notes.push(newNote)
+
+        localStorage.setItem('notes', JSON.stringify(notes))
+        console.log(newNote)
+        popupClose.click()
     }
-    
+
     titleTag.value = ""
     descTag.value = ""
 })
 
-
-let newNote = (title, desc) => {
-    title = title
-    desc = desc
-}
+// DISPLAYING ALL NOTES
