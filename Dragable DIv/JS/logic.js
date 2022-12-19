769dd1417c3edd81d178aedeaf5362137e978804
Leland Capps <lcapps5600@gmail.com>
2022-12-19 13:45:21 -0500

    need to look into how/when to use () in a method

---
+++ Dragable DIv/JS/logic.js
@@ -0,0 +1,47 @@
+const wrapper = document.querySelector('.wrapper')
+const header = wrapper.querySelector('header')
+
+
+
+
+let onDrag = ({movementX, movementY}) => {
+    let getStyle = window.getComputedStyle(wrapper)
+    let top = parseInt(getStyle.top)
+    let left = parseInt(getStyle.left)
+
+    wrapper.style.left = `${left + movementX}px`
+    wrapper.style.top = `${top + movementY}px`
+}
+
+
+header.addEventListener('mousedown', ()=>{
+    header.addEventListener('mousemove', onDrag)
+})
+header.addEventListener('mouseup', ()=>{
+    header.removeEventListener('mousemove', onDrag)
+})
+
+
+
+
+
+
+
+
+
+
+// icon.addEventListener('mousedown', drag())
+// icon.addEventListener('mouseup', () =>{console.log('icon unclickd')})
+
+// let drag = () =>{
+//     getPos()
+//     div.style.left = mousePosX
+//     div.style.top = mousePosY
+// }
+// let getPos = () =>{
+//     let mousePosX = currentMousePos . X coordinate
+//     let mousePosY = currentMousePos . Y coordinate
+
+//     return mousePosX
+//     return mousePosY
+// }
