7fa4d7aa809d0a824b69eb4b4944dfc89282b96c
Leland Capps <lcapps5600@gmail.com>
2022-12-20 16:42:35 -0500

    adding lists of characters to generate the password

---
+++ password Generator/logic.js
@@ -0,0 +1,9 @@
+const passSpan = document.querySelector('#password')
+const alphabet = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "="]
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let pass
+
+let makePassword = () => {
+    console.log(passSpan)
+}
