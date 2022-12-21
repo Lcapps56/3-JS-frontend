eae6945d720c1f1e3b3f97d925b65f4b2bf621a2
Leland Capps <lcapps5600@gmail.com>
2022-12-21 16:56:52 -0500

    base bunctionality workng

--- password Generator/logic.js
+++ password Generator/logic.js
@@ -1,13 +1,27 @@
 const passSpan = document.querySelector('#password')
 const alphabet = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// const alphabet = []
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "="]
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const characters = [...alphabet,...specialChar,...numbers]
 let pass = []
 
+
 let makePassword = () => {
-    alphabet.forEach((letter)=> {
-        pass.push(letter)
-    })
+    let passLength = Math.floor(Math.random()*20)
+    // console.log('button is clicked')
+
+    for (let i=0;i<passLength; i++){
+        pass.push(characters[Math.floor(Math.random() * characters.length)])
+    }
     
-    console.log(pass.toString())
+    console.log(`the password is ${pass}`)
+
+    passSpan.innerHTML = ''
+
+    inputText(pass.join(''))
+}
+
+let inputText = (passwordText) =>{
+    passSpan.innerText = passwordText
 }
