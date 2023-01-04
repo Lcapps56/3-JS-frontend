7408ee2f914d605f87a0dd240321b7fa06d9fb74
Leland Capps <lcapps5600@gmail.com>
2023-01-04 16:54:24 -0500

    starting logic for app

---
+++ Quote of the day/script.js
@@ -0,0 +1,17 @@
+const quoteText = document.querySelector('.quote'),
+authorText = document.querySelector('.name'),
+newQuoteBtn = document.querySelector('.features button'),
+quotes = [
+    {
+        author: '',
+        quote: ''
+    }
+]
+
+newQuoteBtn.addEventListener('click', ()=>{
+    getNewQuote()
+})
+
+let getNewQuote = () => {
+
+}
