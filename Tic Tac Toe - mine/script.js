b867f846f54c58b96252426d5e7f95a9babaa34b
Leland Capps <lcapps5600@gmail.com>
2023-01-11 22:22:00 -0500

    mehod works just need to apply to rest of cases

--- Tic Tac Toe - mine/script.js
+++ Tic Tac Toe - mine/script.js
@@ -11,26 +11,43 @@ const gameVals = [
     false, false, false,
     false, false, false
 ]
+const winningVals = {
+    one: [0, 1, 2],
+    2: [3, 4, 5],
+    3: [6, 7, 8],
+    4: [6, 4, 3], 
+    5: [0, 4, 8],
+    6: [0, 3, 6],
+    7: [1, 4, 5],
+    8: [2, 5, 8]
+}
 
 gameBtns.forEach(function(btn){
     btn.addEventListener('click', function(){
         gameVals[this.value] = true
+        console.log(gameVals)
         checkGameState()
     })
 })
 let checkGameState = () => {
-    checkRows()
-    checkDiags()
-    checkCols()
-    1 2 3
-    4 5 6
-    7 8 9
+    // console.log(gameVals[winningVals.one[0]])
+    
+    // THIS CODE WORKS JUST REPEAT FOR EACH
+    if (gameVals[winningVals.one[0]] && gameVals[winningVals.one[1]] && gameVals[winningVals.one[2]]){
+        console.log("won")
+    }else{
+        console.log('not won')
+    }
 
-    7 5 3
-    1 5 9
 
-    1 4 7
-    2 5 6
-    3 6 9
+
+
+
+
+
+
+    // for (let i=0; i<winnigVals.length; i++){
+        
+    // }
 
 }
