6a52b8dea183cd45aacfe75f777b87eba9df18c9
Leland Capps <lcapps5600@gmail.com>
2023-01-13 15:50:07 -0500

    start game btn works

--- Tic Tac Toe - mine/script.js
+++ Tic Tac Toe - mine/script.js
@@ -1,5 +1,5 @@
-const startBtn = document.querySelector('.start'),
-gameRunning = false
+const startBtn = document.querySelector('.start')
+let gameRunning = false
 let gameBtns = document.querySelector('.game').getElementsByTagName('button')
 
 gameBtns = Array.from(gameBtns)
@@ -14,33 +14,42 @@ const winningVals = {
     one: [0, 1, 2],
     two: [3, 4, 5],
     three: [6, 7, 8],
-    four: [6, 4, 3], 
+    four: [6, 4, 2], 
     five: [0, 4, 8],
     six: [0, 3, 6],
     seven: [1, 4, 5],
     eight: [2, 5, 8]
 }
 
-
-
-
-gameBtns.forEach(function(btn){
-    btn.addEventListener('click', function(){
-        gameVals[this.value] = true
-        console.log(gameVals)
-        checkGameState()
-    })
+startBtn.addEventListener('click', () => {
+    clearBoard()
+    gameRunning = true
+    gameBtns.forEach(function(btn){
+        btn.addEventListener('click', function(){
+            gameVals[this.value] = true
+            console.log(gameVals)
+            checkGameState()
+        })
+    }) 
 })
+
 let checkGameState = () => {
-    for (val in winningVals){
-        console.log(winningVals[val][0])
-        
-        if(gameVals[winningVals[val][0]] && gameVals[winningVals[val][1]] && gameVals[winningVals[val][2]]){
-            alert('game won')
-            gameRunning = false
-        }else{
+    if(gameRunning){
+        for (val in winningVals){
+            if(gameVals[winningVals[val][0]] && gameVals[winningVals[val][1]] && gameVals[winningVals[val][2]]){
+                alert('game won')
+                gameRunning = false
+                location.reload()
+                // continue
+            }else{
+            }
         }
+    }else{
     }
 }
 
-
+function clearBoard () {
+    gameBtns.forEach(function(btn){
+        btn.innerHTML = ''
+    })
+}
